da67485af15fd203a3bf8495e9bd7338b156371e
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2019-08-18 22:56:23 +0700

    [ADD] migrate

---
+++ src/db/migrations/20190818201447_create_table_user.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', function(t) {
+    t.increments('id')
+      .unsigned()
+      .primary();
+    t.dateTime('createdAt').notNull();
+    t.dateTime('updatedAt').nullable();
+    t.dateTime('deletedAt').nullable();
+
+    t.string('firstName').nullable();
+    t.string('lastName').nullable();
+    t.string('email').nullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
