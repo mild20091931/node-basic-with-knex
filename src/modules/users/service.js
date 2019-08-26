57a9bad38d3e6d74965be9776c531e730fa38316
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-08-26 16:19:03 +0700

    [UPDATE] add method checkdatanotnull & genpassword :new_moon_with_face:

--- src/modules/users/service.js
+++ src/modules/users/service.js
@@ -1,3 +1,10 @@
+export const checkDataNotNull = data => {
+  if (data[0] === undefined){
+    return false;
+  }
+  return true;
+}
+
 const UserService = {
   reverseFullName: user => {
     const userJson = JSON.stringify(user[0]);
@@ -11,6 +18,18 @@ const UserService = {
     } else {
       return '';
     }
+  },
+
+  genPassWord: user => {
+    console.log(user[0] === undefined);
+      if(checkDataNotNull(user)){
+        const userJson = JSON.stringify(user[0]);
+        const random = String(Math.random().toString(36).toUpperCase().substring(3,6)
+        + Math.random().toString(36).substring(4,7));
+        return random;
+      }else{
+        return "invalid"
+      }
   },
 };
 export default UserService;
