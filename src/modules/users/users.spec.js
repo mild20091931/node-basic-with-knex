f73e3712018751c8adcc7c60b0f7cc07005ee0ec
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-08-26 16:19:46 +0700

    [UPDATE] add test case & null mockuser :new_moon_with_face:

--- src/modules/users/users.spec.js
+++ src/modules/users/users.spec.js
@@ -1,4 +1,5 @@
-import userService from './service';
+import userService,{ checkDataNotNull } from './service';
+import { testNameToKey } from 'jest-snapshot/build/utils';
 
 describe('UserService-Test', () => {
   const mockUser = [
@@ -10,14 +11,47 @@ describe('UserService-Test', () => {
       firstName: 'Mild',
       lastName: 'Noita',
       email: 'nakaz@email.com',
-    },
-  ];
+    }]
+    const mockUser1 = [{
+      id: 3,
+      createdAt: '2019-08-18T16:48:38.000Z',
+      updatedAt: null,
+      deletedAt: null,
+      firstName: 'Test2',
+      lastName: 'Noita',
+      email: 'nakaz@email.com',
+    }]
+    const mockUser2 = [];
+
   describe('reverseFullName()', () => {
     test("should not return ''", () => {
       expect(userService.reverseFullName(mockUser)).not.toBe('');
     });
-    test('should be return "dliM" when given "Mild"', () => {
+    test('should be return "atioNdliM" when given "MildNoita"', () => {
       expect(userService.reverseFullName(mockUser)).toBe('atioNdliM');
     });
+    test('should be return "atioN2tseT" when given "Test2Noita"', () => {
+      expect(userService.reverseFullName(mockUser1)).toBe('atioN2tseT');
+    });
+  });
+
+  describe('genPassWord()', () => {
+    test("should return invalid was given mockup data not found ", () => {
+      expect(userService.genPassWord(mockUser2)).toBe('invalid');
+    });
+    test("should return password was generate when given mockup data", () => {
+      expect(typeof userService.genPassWord(mockUser) === "string").toBe(true);
+    });
+  });
+
+  describe('checkDataNotNull', () => {
+    test('should return false when given data[0] is undefined', () => {
+      expect(checkDataNotNull(mockUser2)).toBe(false);
+    });
+    test('should return true when given data is not null', () => {
+      expect(checkDataNotNull(mockUser1)).toBe(true);
+    })
+    
+    
   });
 });
