2cbfb432f241a58a6ecdd03507ed10669c9f24f9
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-08-26 16:25:06 +0700

    [UPDATE] add path for genpasswd :new_moon_with_face:

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -4,5 +4,6 @@ import userController from './controller';
 const routes = express.Router();
 
 routes.get('/:firstName', userController.reverseFullName);
+routes.get('/genpwd/:firstName', userController.genPassWordController);
 
 export default routes;
