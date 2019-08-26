79dd63958a85cf26f90e9d11d3fd35e4c015ddc2
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-08-26 16:17:26 +0700

    [UPDATE] add method genpassword on controller :new_moon_with_face:

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -8,4 +8,11 @@ export default {
     const reverseFullName = await userService.reverseFullName(user);
     return res.send(reverseFullName);
   },
+
+  genPassWordController: async (req, res) => {
+    const firstName = await req.params.firstName;
+    const user = await userModel.findUserByFirstName(firstName);
+    const genPassWord = await userService.genPassWord(user);
+    return res.send(genPassWord);
+  },
 };
