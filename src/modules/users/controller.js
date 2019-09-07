5ab05b1678b994b1c7be67961d63068d7c92d8d6
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-09-07 13:27:44 +0700

    [UPDATE] Add function newuser :new_moon_with_face:

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -2,9 +2,10 @@ import userModel from './model';
 import userService from './service';
 
 export default {
-  reverseFullName: async (req, res) => {
+  reverseFullNameController: async (req, res) => {
     const firstName = await req.params.firstName;
     const user = await userModel.findUserByFirstName(firstName);
+    console.log(user);
     const reverseFullName = await userService.reverseFullName(user);
     return res.send(reverseFullName);
   },
@@ -14,5 +15,12 @@ export default {
     const user = await userModel.findUserByFirstName(firstName);
     const genPassWord = await userService.genPassWord(user);
     return res.send(genPassWord);
+  },
+
+  newUserController: async (req, res) => {
+    const data = await req.body;
+    const newUser = await userModel.createUser(data);
+    return res.status(201).send('Success!');
+    console.log(data);
   },
 };
