3824f9ee4097da8bf81c49c079d8d4decfa13614
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-09-07 13:29:18 +0700

    [UPDATE] add route newuser :new_moon_with_face:

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -3,7 +3,8 @@ import userController from './controller';
 
 const routes = express.Router();
 
-routes.get('/:firstName', userController.reverseFullName);
+routes.get('/:firstName', userController.reverseFullNameController);
 routes.get('/genpwd/:firstName', userController.genPassWordController);
+routes.post('/:newuser', userController.newUserController);
 
 export default routes;
