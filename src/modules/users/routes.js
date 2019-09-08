36b7c248e4b371b0a5b21fe75f341e5ae145b4ea
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-09-08 11:39:39 +0700

    [UPDATE] add update route :new_moon_with_face:

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -6,5 +6,6 @@ const routes = express.Router();
 routes.get('/:firstName', userController.reverseFullNameController);
 routes.get('/genpwd/:firstName', userController.genPassWordController);
 routes.post('/:newuser', userController.newUserController);
+routes.put('/update/:id', userController.updateAllController);
 
 export default routes;
