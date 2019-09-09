b00fb5ea79fbb7de627d3310ae668f36f3cff7c3
kaotu <patthraporn.tu@mail.kmutt.ac.th>
2019-09-09 13:31:14 +0700

    [UPDATE] add route delete :new_moon_with_face:

--- src/modules/users/routes.js
+++ src/modules/users/routes.js
@@ -7,5 +7,6 @@ routes.get('/:firstName', userController.reverseFullNameController);
 routes.get('/genpwd/:firstName', userController.genPassWordController);
 routes.post('/:newuser', userController.newUserController);
 routes.put('/update/:id', userController.updateAllController);
+routes.delete('/delete/:id', userController.deleteUserController);
 
 export default routes;
